19a69b2e7f5bdbf28d6bd23844828e09c8f218bc
phantom 0x7ce <phantom@phantom.localhost>
2019-01-17 13:19:53 -0400

    Working on install packages functions

--- package.py
+++ package.py
@@ -1,6 +1,8 @@
 from utils import download_http
 from parser import line_list
 from random import getrandbits
+from shutil import copyfile
+from subprocess import run
 import tarfile
 import os
 
@@ -36,3 +38,16 @@ def install(db, pkgname):
         for required_package in required_list:
             install(db, required_package)
     # Download package if there's no dependency
+    # Download from https://ftp.slackbuilds.org/pub/slackbuilds/14.2/<packagename>.tar.gz
+    path = download_http('https://ftp.slackbuilds.org/pub/slackbuilds/14.2'+dic[0]['SLACKBUILD LOCATION'][0].lstrip('.').rstrip('/')+'.tar.gz', 'pkgs/slackbuild/')
+    tf = tarfile.open(path)
+    tf.extractall('pkgs/slackbuild/')
+    download = dic[0]['SLACKBUILD DOWNLOAD']
+    if(not download or download[0] == 'UNSUPPORTED'):
+        download = dic[0]['SLACKBUILD DOWNLOAD_x86_64']
+    for src in download:
+        download_http(src, 'pkgs/src/')
+        slackbuild_dir = 'pkgs/slackbuild/'+dic[0]['SLACKBUILD NAME'][0]
+        copyfile('pkgs/src/'+os.path.basename(src), slackbuild_dir+'/'+os.path.basename(src))
+        print('Running %s' % dic[0]['SLACKBUILD NAME'][0])
+        run([slackbuild_dir + '/' + dic[0]['SLACKBUILD NAME'][0] + '.SlackBuild'])
