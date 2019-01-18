12e69cf15232d63f139152128414c64d38871e9b
Matheus Garcias <matheus_garcias@hotmail.com>
2019-01-18 14:18:20 -0400

    Trying to parse .SlackBuilds to get final package name

--- parser.py
+++ parser.py
@@ -19,3 +19,24 @@ def line(string, separator, dic):
 def line_list(lst, separator, dic):
     for i in lst:
         line(i, separator, dic)
+
+def slackbuildscript(path):
+    dic = {}
+    with open(path, 'rt') as f:
+        line = f.readline()
+        while('PRGNAM' not in dic or 'VERSION' not in dic or 'BUILD' not in dic or 'TAG' not in dic):
+            if(line[0] == '#' or line == '\n'):
+                line = f.readline()
+                continue
+            s = line.split('=')
+            if(s[0] == 'PRGNAM' or s[0] == 'VERSION' or s[0] == 'BUILD' or s[0] == 'TAG'):
+                dic[s[0]] = s[1].strip()
+            line = f.readline()
+    clear = lambda string: string.split(':')[1].strip('}')
+    filename = "%s%s-x86_64%s%s.tgz" % (dic['PRGNAM'], clear(dic['VERSION']), clear(dic['BUILD']), clear(dic['TAG']).strip('-'))
+    return filename
+    
+if __name__ == '__main__':
+    import sys
+    slackbuildscript(sys.argv[1])
+            
